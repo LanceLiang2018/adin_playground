f3c38adbb4e973787d02707f7c21c2bc48937a02
ckh0 <weizhaoxiangsb@hotmail.com>
2018-05-24 22:29:02 +0800

    first available version

--- wifidisp/esp8266/esp8266_wifidisp/esp8266_wifidisp.ino
+++ wifidisp/esp8266/esp8266_wifidisp/esp8266_wifidisp.ino
@@ -3,9 +3,9 @@
 #include <U8g2lib.h>
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-const char* ssid     = "LTYZ-EDU";
-const char* password = "ltyz13579";
-const char* host = "172.16.20.36";
+const char* ssid     = "your_ssid";
+const char* password = "your_password";
+const char* host = "your_domain";
 const int port = 6666;
 WiFiClient client;
 
@@ -27,32 +27,41 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  while (!client.connected()){ //连接TCP服务器
+  while (!client.connected()){ 
     if (!client.connect(host, port)){
       Serial.println("Connect failed, retrying...");
       client.stop();
       delay(500);
     }
   }
+  Serial.println("Connected!");
+  u8g2.begin();
+  delay(500);
+  client.println("n");
 }
 
-unsigned int img[1024];
+unsigned char img[1024];
 int i = 0;
 
 void loop() {
   while (client.available()){
     if (i == 1024){
       i = 0;
+      client.println("n"); //ask for next frame
       break;
     }
     img[i] = client.read();
+    //Serial.write(img[i]);
+    i++;
   }
+  
 
   u8g2.firstPage();
   do {
-    u8g2.drawXBMP(0,0, 128, 64, (uint8_t*)&img);
+    u8g2.drawXBMP(0,0, 128, 64, img);
   } while ( u8g2.nextPage() );
+  
 
-  client.println("n"); //显示完成，请求下一帧
+  
 }
 
--- wifidisp/py_windows/server.py
+++ wifidisp/py_windows/server.py
@@ -6,7 +6,8 @@ def pixelToChr(pixels):
     count = 0
     bits = 0
     for pixel in pixels:
-        (b, g, r) = pixel
+        #(b, g, r) = pixel
+        b = pixel
         '''if b < 127:
             p = 128
         else:
@@ -19,29 +20,30 @@ def pixelToChr(pixels):
         p = p << count
         bits = bits | p
         count += 1
-    #return bits
-    return chr(bits)
+    return bits
+    #return chr(bits)
     
 def scanFrame(frame):
-    (y, x, channel) = frame.shape
-    Str = ''
+    #(y, x, channel) = frame.shape
+    (y, x) = frame.shape
     chs = []
     for yy in range(y):
         for x0 in range(0, x, 8):
             pixels = []
             for i in range(8):
                 pixels.append(frame[yy, x0 + i])
-            #chs.append(pixelToChr(pixels))
-            Str += pixelToChr(pixels)
+            chs.append(pixelToChr(pixels))
+            #Str += pixelToChr(pixels)
         yy += 1
-    #return chs
-    return Str
+    return chs
+    #return Str
 
 
 
 #main
 filename = input('filename>')
 cap = cv2.VideoCapture(r'E:\\orai\\' + filename)
+print(str(cap.get(7)) + " frames") #number of frames in the video file
 
 host = socket.gethostname()
 port = int(input('port>'))
@@ -51,13 +53,14 @@ s.listen(1)
 print('waiting for client...')
 cliSock, cliAddr = s.accept()
 print(str(cliAddr) + ' connected')
-
+cliSock.recv(512) 
 t = int(input("number of flames to play>"))
 for i in range(t+1):
     ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.resize(frame, (128,64))
-    #cv2.imshow(filename, frame)
-    cliSock.send(scanFrame(frame).encode())
+    cliSock.send(bytes(scanFrame(frame)))
+    cliSock.recv(512)
 print('goodbye')
     
 
